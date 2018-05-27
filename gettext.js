65b2490fd4de93dc39d6d58f5b03b2c9398b9af1
miv-shar <miv-shar@yandex.ua>
2018-05-27 15:41:40 +0300

    lab3

---
+++ gettext.js
@@ -0,0 +1,11 @@
+function getPoetry(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "poetry.txt", true);
+  xhttp.send();
+}
