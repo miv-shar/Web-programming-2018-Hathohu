76935043047a1a2c10e777ce4f87c0c5ce6a8915
miv-shar <miv-shar@yandex.ua>
2018-05-28 11:03:42 +0300

    first js try

--- ajax-reader.js
+++ ajax-reader.js
@@ -1,3 +1,17 @@
-function GetText{
-	
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
