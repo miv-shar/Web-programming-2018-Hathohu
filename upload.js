b514e762213c296a50b9f380a7637f0b0baeaf9b
miv-shar <miv-shar@yandex.ua>
2018-05-29 06:04:12 +0300

    29.05.18.1 upload

---
+++ upload.js
@@ -0,0 +1,26 @@
+function upload(file) {
+
+  var xhr = new XMLHttpRequest();
+  xhr.upload.onprogress = function(event) {
+    document.getElementById('progress').value = event.loaded/event.total*100;
+  }
+  xhr.onload = xhr.onerror = function() {
+    if (this.status == 200) {
+      log("success");
+    } else {
+      log("error " + this.status);
+    }
+  };
+  xhr.open("POST", "upload", true);
+  xhr.send(file);
+
+}
+function invokeUpload(){
+  document.forms.upload.onsubmit = function() {
+    var input = this.elements.myfile;
+    var file = input.files[0];
+    if (file) {
+      upload(file);
+    }
+    return false;
+	}
