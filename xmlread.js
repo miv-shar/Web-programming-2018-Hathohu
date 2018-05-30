0804af04bdec06aac7681b18ece5e70d9c4bad41
miv-shar <miv-shar@yandex.ua>
2018-05-31 02:53:32 +0300

    31.05.18.3

--- xmlread.js
+++ xmlread.js
@@ -1,18 +1,26 @@
-doc=HUGOWINTBL.XMLDocument;
-if(doc.readyState == 4) DisplayError();
-else doc.onreadystatechange = DisplayError;
-function DisplayError(){
-    if(doc.readyState != 4) return;
-    if(doc.parseError.errorCode){
-        mess =
-        "parseError.errorCode: " + doc.parseError.errorCode + "\n" +
-        "parseError.filepos: " + doc.parseError.filepos + "\n" +
-        "parseError.line: " + doc.parseError.line + "\n" +
-        "parseError.linepos: " + doc.parseError.linepos + "\n" +
-        "parseError.reason: " + doc.parseError.reason + "\n" +
-        "parseError.srcText: " + doc.parseError.srcText + "\n" +
-        "parseError.url: " + doc.parseError.url + "\n" +
-        "";
-        alert(mess);
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
     }
+  };
+  xmlhttp.open("GET", "hugo-winners.xml", true);
+  xmlhttp.send();
+}
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>Type</th><th>Works Name</th><th>Author</th></tr>";
+  var x = xmlDoc.getElementsByTagName("row");
+  for (i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("type")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("wname")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+	x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("HugoTable").innerHTML = table;
 }
