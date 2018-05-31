82c0e68acdebede4b8f7db03ee701e2429a26dc8
miv-shar <miv-shar@yandex.ua>
2018-05-31 12:56:44 +0300

    27

--- js/jsonread.js
+++ js/jsonread.js
@@ -1,10 +1,29 @@
+// function rjson(){
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+    // var txt = "";
+	// var x;
+	// if (this.readyState === 4 && this.status === 200) {
+        // var myObj = JSON.parse(this.responseText);
+        // txt += "<table id='SejunTable' class='ha'>";
+        // for (x in myObj) {
+            // txt += "<tr><td>" + myObj[x].type + "</td><td>" +
+			// + myObj[x].wname + "</td><td>" +
+            // + myObj[x].author + "</td></tr>";
+        // }
+        // txt += "</table>" 
+        // document.getElementById("jsonTable").innerHTML = txt;
+    // };
+// }
+// xhr.open("GET", "sejun.json", true);
+// xhr.send();
+// }
 function rjson(){
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    var txt = "";
-	var x;
-	if (this.readyState === 4 && this.status === 200) {
-        var myObj = JSON.parse(this.responseText);
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		var myObj = JSON.parse(this.responseText);
+		var x, txt;
         txt += "<table id='SejunTable' class='ha'>";
         for (x in myObj) {
             txt += "<tr><td>" + myObj[x].type + "</td><td>" +
@@ -12,9 +31,10 @@ xhr.onreadystatechange = function() {
             + myObj[x].author + "</td></tr>";
         }
         txt += "</table>" 
-        document.getElementById("jsonTable").innerHTML = txt;
-    };
-}
-xhr.open("GET", "sejun.json", true);
-xhr.send();
-}
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("jsonTable").innerHTML = myObj.name;
+		}
+	};
+	xmlhttp.open("GET", "sejun.txt", true);
+	xmlhttp.send();
+	}
