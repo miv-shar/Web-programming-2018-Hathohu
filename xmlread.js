c51dbdcecfc2977385b6b67235021fdb3606a260
miv-shar <miv-shar@yandex.ua>
2018-05-31 05:10:46 +0300

    if it doesn't work, this commit probably broke it

--- xmlread.js
+++ xmlread.js
@@ -1,12 +1,13 @@
 function loadXMLDoc() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       myFunction(this);
     }
-  };
-  xmlhttp.open("GET", "hugo-winners.xml", true);
-  xmlhttp.send();
+  };    
+  xhr.Response.Cache.SetCacheability(HttpCacheability.NoCache); 
+  xhr.open("GET", "hugo-winners.xml", true);
+  xhr.send();
 }
 function myFunction(xml) {
   var i;
