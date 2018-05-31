cabe9222dfa956ee76bee4231bff7c71e725326c
miv-shar <miv-shar@yandex.ua>
2018-05-31 10:33:49 +0300

    upload 20

--- upload.js
+++ upload.js
@@ -3,14 +3,8 @@ function upload(file) {
   var xhr = new XMLHttpRequest();
   xhr.upload.onprogress = function(event) {
     document.getElementById('progress').value = event.loaded/event.total*100;
+	}
   }
-  // xhr.onload = xhr.onerror = function() {
-    // if (this.status == 200) {
-      // log("success");
-    // } else {
-      // log("error " + this.status);
-    // }
-  };
   xhr.open("POST", "upload", true);
   xhr.send(file);
 
