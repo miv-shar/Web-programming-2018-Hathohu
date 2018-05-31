0ec311387bc6b196276010431a01b356e4fbc986
miv-shar <miv-shar@yandex.ua>
2018-05-31 13:50:02 +0300

    json WORKED, at last, at this point, in case of breking go here

--- js/jsonread.js
+++ js/jsonread.js
@@ -1,23 +1,3 @@
-// function rjson(){
-// var xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function() {
-    // var txt = "";
-	// var x;
-	// if (this.readyState === 4 && this.status === 200) {
-        // var myObj = JSON.parse(this.responseText);
-        // txt += "<table id='SejunTable' class='ha'>";
-        // for (x in myObj) {
-            // txt += "<tr><td>" + myObj[x].type + "</td><td>" +
-			// + myObj[x].wname + "</td><td>" +
-            // + myObj[x].author + "</td></tr>";
-        // }
-        // txt += "</table>" 
-        // document.getElementById("jsonTable").innerHTML = txt;
-    // };
-// }
-// xhr.open("GET", "sejun.json", true);
-// xhr.send();
-// }
 function rjson(){
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
