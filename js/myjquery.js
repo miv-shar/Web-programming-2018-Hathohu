0103445f61d439085b72078a9fb33c2c1cf2ca44
miv-shar <miv-shar@yandex.ua>
2018-06-04 09:25:11 +0300

    temp new index,

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,6 +1,6 @@
     $.fn.myFunction = function(){ 
         alert('You were told not to push this button!\nYou will be punished!'); 
-		$('body').css("display", "none");
+		$("body").hide(1000);
     }
 /*this jQuery function punishes you,
 by hiding the page entierly. 
